1004737cbd61a024962576aa8b03f48ce6b1c50a
Qurbonov <Qurbonov@myPC>
2016-03-04 10:39:03 -0800

    04.03.16 backup - certificate entity added

---
+++ src/main/java/mf/uz/domain/Certificate.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mf.uz.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author qurbonov
+ */
+@Entity
+@Table(name = "certificates")
+public class Certificate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CertificateSeq")
+    @Column(name = "C_ID", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "EMPLOYEE_ID")
+    private Users user;
+
+    @Column(name = "CONTENT")
+    private byte[] content;
+
+    @Column(name = "CHECKSUM_MD5")
+    private String checksumMd5;
+
+    @Column(name = "dateTo")
+    private Date dateTo;
+
+    @Column(name = "dateFrom")
+    private Date dateFrom;
+
+    public Certificate() {
+    }
+
+    public Certificate(Long id, Users user, byte[] content, String checksumMd5, Date dateTo, Date dateFrom) {
+        this.id = id;
+        this.user = user;
+        this.content = content;
+        this.checksumMd5 = checksumMd5;
+        this.dateTo = dateTo;
+        this.dateFrom = dateFrom;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getChecksumMd5() {
+        return checksumMd5;
+    }
+
+    public void setChecksumMd5(String checksumMd5) {
+        this.checksumMd5 = checksumMd5;
+    }
+
+    public Date getDateTo() {
+        return dateTo;
+    }
+
+    public void setDateTo(Date dateTo) {
+        this.dateTo = dateTo;
+    }
+
+    public Date getDateFrom() {
+        return dateFrom;
+    }
+
+    public void setDateFrom(Date dateFrom) {
+        this.dateFrom = dateFrom;
+    }
+}
